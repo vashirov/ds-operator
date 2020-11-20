a63f1312884aab2fa24de1748df2bec6c1e74d6f
Viktor Ashirov <vashirov@redhat.com>
2020-11-20 13:51:54 +0100

    Downgrade api version to v1alpha1
    We're not v1 yet...

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	idmv1 "github.com/vashirov/ds-operator/api/v1"
+	idmv1alpha1 "github.com/vashirov/ds-operator/api/v1alpha1"
 	"github.com/vashirov/ds-operator/controllers"
 	// +kubebuilder:scaffold:imports
 )
@@ -40,7 +40,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(idmv1.AddToScheme(scheme))
+	utilruntime.Must(idmv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
